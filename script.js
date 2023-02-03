25eda211f7d8e36c7dc5b927596a65445d0d9769
Phoenix1604 <subhajit.bera@wisdmlabs.com>
2023-02-03 14:31:25 +0530

    hero section responsiveness fixed

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector('#menu').addEventListener('click', function onClick() {
     document.querySelector('nav ul').classList.toggle('showmenu');
 })
+
+const loadToolPage = () => {
+    return window.location.href = "./toolpage.html";
+}
